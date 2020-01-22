3bf6858b29dc80884d5206947d3b2c007599ad02
SilverPigtail <3512126@gmail.com>
2020-01-22 11:18:32 +0100

    Started the tiled designs
    - Used TiledMap to create some maps with different sizes.

---
+++ Resources/Assets/Maps_Atlas/tsxFiles/MageCity20x20.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="MageCity20x20" tilewidth="20" tileheight="20" tilecount="864" columns="12">
+ <image source="../atlasPNG/magecity.png" width="256" height="1450"/>
+</tileset>
