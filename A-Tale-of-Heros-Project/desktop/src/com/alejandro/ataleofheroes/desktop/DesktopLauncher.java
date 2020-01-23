aaf5bc4d8de993515cff0447b330e80197914b23
SilverPigtail <3512126@gmail.com>
2020-01-23 13:46:22 +0100

    Recreated the LibGDX project
    - Created the project again. This time it has the Manifest file.

---
+++ A-Tale-of-Heros-Project/desktop/src/com/alejandro/ataleofheroes/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.alejandro.ataleofheroes.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.alejandro.ataleofheroes.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+	}
+}
