dc75e8ddfcbe51b5b219c11a6acea55173734872
SilverPigtail <3512126@gmail.com>
2020-01-24 01:08:15 +0100

    Started coding the project.
    - Started creating the all the classes and trying to implement the tiled map.

--- A-Tale-of-Heros-Project/core/src/com/alejandro/ataleofheroes/MainGameClass.java
+++ A-Tale-of-Heros-Project/core/src/com/alejandro/ataleofheroes/MainGameClass.java
@@ -5,15 +5,32 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class MainGameClass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
+	private TiledMap map;
+	private OrthogonalTiledMapRenderer oRenderer;
+
+	public static final float unitScale = 1/16f;
+
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		/*batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");*/
+
+		float widthMap = Gdx.graphics.getWidth();
+		float heightMap = Gdx.graphics.getHeight();
+
+		map = new TmxMapLoader().load("maps/TownMap.tmx");
+
+		oRenderer = new OrthogonalTiledMapRenderer(map, unitScale);
+
 	}
 
 	@Override
---
+++ A-Tale-of-Heros-Project/core/src/com/alejandro/ataleofheroes/constants/Constants.java
@@ -0,0 +1,5 @@
+package com.alejandro.ataleofheroes.constants;
+
+public class Constants {
+    public final static double gameVersion = 0.05f;
+}
