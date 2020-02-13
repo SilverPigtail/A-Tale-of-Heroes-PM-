84ca4d85ebe6219ee26dd7e05c0ce440aefcaf28
Silver (Ale) <3512126@gmail.com>
2020-02-13 14:56:01 +0100

    - Starting codding the main menú.
    - Starting codding the main menu of the game using activities coded in Java.

--- A-Tale-of-Heros-Project/android/src/com/alejandro/ataleofheroes/GameMenu.java
+++ A-Tale-of-Heros-Project/android/src/com/alejandro/ataleofheroes/GameMenu.java
@@ -1,5 +1,6 @@
 package com.alejandro.ataleofheroes;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class GameMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_menu);
+
+        ConstraintLayout mainLayout = (ConstraintLayout)findViewById(R.id.mainConstraintL);
+
+        
     }
 }
