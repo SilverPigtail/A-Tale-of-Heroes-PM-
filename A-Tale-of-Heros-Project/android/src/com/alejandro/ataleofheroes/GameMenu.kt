eb3bb8b3c71ba6e9cc7f61b788a11d1573a4a0fa
SilverPigtail <3512126@gmail.com>
2020-03-02 19:54:02 +0100

    - Exit window.
    - Added the function that allows the user to close the app from the activity just pressing the back button from Android.

--- A-Tale-of-Heros-Project/android/src/com/alejandro/ataleofheroes/GameMenu.kt
+++ A-Tale-of-Heros-Project/android/src/com/alejandro/ataleofheroes/GameMenu.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.constraint.ConstraintLayout
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Process
+import android.support.v7.app.AlertDialog
 import android.view.View
 import services.AlertService
 
@@ -32,6 +34,31 @@ class GameMenu : AppCompatActivity() {
         val i = Intent(this, AndroidLauncher::class.java)
         stopService(intentS)
         this.startActivity(i)
+
+    }
+
+    /***
+     * This funcition allows the user to close the app from the activity just pressing the back button from Android.
+     */
+    override fun onBackPressed() {
+        var title: String = "Exit the game"
+        var message: String = "Are you sure that do you want to exit the game?"
+        var rYes: String = "Yes"
+        var rNo: String = "No"
+
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+
+        builder.setMessage(message).setPositiveButton(rYes) {
+            dialog, id -> Process.killProcess(Process.myPid())
+            System.exit(1)
+
+        }.setNegativeButton(rNo) { dialog, id -> }
+
+        builder.create()
+        builder.show()
+
 
     }
 }
